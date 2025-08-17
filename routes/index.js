c7bbc5adf8548a55516d4e63b0112a5e96a4f5f1
pataegonia <clue9986@gmail.com>
2025-08-17 20:25:16 +0900

    updating profile photo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import authRoute from './authRoutes.js';
+import uploadRoutes from './uploadRoutes.js';
 
 const router = Router();
 
 router.get('/health', (req, res) => res.json({ ok: true, ts: Date.now() }));
 router.use('/user', authRoute);
+router.use('/', uploadRoutes);
 
 export default router;
