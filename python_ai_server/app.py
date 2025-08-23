defad934ef866411e83bf8c99ac7e89c22fa961e
SHINJINSU <jsshin8128@gmail.com>
2025-08-23 13:44:46 +0900

    feat:fastapi 서버 cors 허용

--- python_ai_server/app.py
+++ python_ai_server/app.py
@@ -1,4 +1,6 @@
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 from langchain_logic import get_place_recommendations
@@ -9,7 +11,17 @@ from dotenv import load_dotenv
 env_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
 load_dotenv(env_path)
 
+
 app = FastAPI()
+
+# CORS 허용
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class RecommendRequest(BaseModel):
     location: str
